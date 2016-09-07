3154325bac5d6eb735b3859c709a36f38f8c00f3
Matt Dawkins <matt.dawkins@kitware.com>
2016-09-07 16:56:53 -0400

    Update for latest VXL updates

--- Libraries/VgVideo/vslInstantiations.cxx
+++ Libraries/VgVideo/vslInstantiations.cxx
@@ -4,8 +4,8 @@
  * Kitware, Inc., 28 Corporate Drive, Clifton Park, NY 12065.
  */
 
-#include <vnl/io/vnl_io_vector_fixed.txx>
-#include <vsl/vsl_vector_io.txx>
+#include <vnl/io/vnl_io_vector_fixed.hxx>
+#include <vsl/vsl_vector_io.hxx>
 
 VNL_IO_VECTOR_FIXED_INSTANTIATE(double, 2);
 VSL_VECTOR_IO_INSTANTIATE(vnl_vector_fixed<double VCL_COMMA 2>);
