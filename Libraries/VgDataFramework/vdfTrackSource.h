d3b35b7acd8a583e33c70b1ed004cb1079295c9c
Matthew Woehlke <matthew.woehlke@kitware.com>
2018-05-17 16:49:31 -0400

    Make trackClassificationAvailable accessible
    Add trackClassificationAvailable to the set of signals that
    vdfTrackSource makes available to track sources (by reclassifying them
    from protected to public). Also, add metatype registration for its
    parameter type, vvTrackClassification. Apparently, no one has needed to
    emit this before...

--- Libraries/VgDataFramework/vdfTrackSource.h
+++ Libraries/VgDataFramework/vdfTrackSource.h
@@ -1,5 +1,5 @@
 /*ckwg +5
- * Copyright 2013 by Kitware, Inc. All Rights Reserved. Please refer to
+ * Copyright 2018 by Kitware, Inc. All Rights Reserved. Please refer to
  * KITWARE_LICENSE.TXT for licensing information, or contact General Counsel,
  * Kitware, Inc., 28 Corporate Drive, Clifton Park, NY 12065.
  */
@@ -107,6 +107,7 @@ public:
   virtual ~vdfTrackSource();
 
   using vdfTrackSourceInterface::trackNameAvailable;
+  using vdfTrackSourceInterface::trackClassificationAvailable;
   using vdfTrackSourceInterface::trackUpdated;
   using vdfTrackSourceInterface::trackClosed;
 
