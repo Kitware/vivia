420ebf4a2d83dedba72beacf0ecef43dda7fdb52
Matthew Woehlke <matthew.woehlke@kitware.com>
2018-10-09 17:02:28 -0400

    Refactor reading frames from a KWA
    Extract the logic to read a single frame image from a KWA into a
    separate helper function. This will allow us to reuse the code (and
    also, to avoid needing to make vgIStream a public class) for the
    upcoming VTK KWA reader.

--- Libraries/VgVideo/vgKwaUtil.cxx
+++ Libraries/VgVideo/vgKwaUtil.cxx
@@ -1,20 +1,51 @@
 /*ckwg +5
- * Copyright 2013 by Kitware, Inc. All Rights Reserved. Please refer to
+ * Copyright 2018 by Kitware, Inc. All Rights Reserved. Please refer to
  * KITWARE_LICENSE.TXT for licensing information, or contact General Counsel,
  * Kitware, Inc., 28 Corporate Drive, Clifton Park, NY 12065.
  */
 
-#include "vgKwaUtil.h"
+#include "vgKwaUtilPrivate.h"
+
+#include <vgDebug.h>
+
+#include <vil/file_formats/vil_jpeg.h>
+#include <vil/vil_stream_core.h>
+
+#include <vil/io/vil_io_image_view.h>
+#include <vsl/vsl_binary_io.h>
+#include <vsl/vsl_stream.h>
+#include <vsl/vsl_vector_io.h>
+#include <vsl/vsl_vector_io.hxx>
 
 #include <QDebug>
 #include <QDir>
 #include <QFile>
 #include <QFileInfo>
+#include <QPair>
+
+#include <vector>
+
+VSL_VECTOR_IO_INSTANTIATE(char);
 
 #define die(...) do { \
   qDebug() << "vgKwaUtil:" << __VA_ARGS__; \
   return false; \
   } while (0)
+
+namespace // anonymous
+{
+
+//-----------------------------------------------------------------------------
+void vilCleanup(void* data)
+{
+  // Destroy our memory chunk pointer, which will cleanly release our reference
+  // to the memory chunk
+  vil_memory_chunk_sptr* chunk =
+    reinterpret_cast<vil_memory_chunk_sptr*>(data);
+  delete chunk;
+}
+
+} // namespace <anonymous>
 
 //-----------------------------------------------------------------------------
 bool vgKwaUtil::resolvePath(
@@ -68,4 +99,90 @@ bool vgKwaUtil::resolvePath(
       << "\n  Tried:" << try1
       << "\n        " << try2
       << "\n        " << try3);
+}
+
+//-----------------------------------------------------------------------------
+QPair<long long, vgImage> vgKwaUtil::readFrame(
+    vgIStream* store, vsl_b_istream* stream, int version, long long offset,
+    const char* warningPrefix, const vgTimeStamp& expectedTime)
+{
+  // Seek stream to appropriate location
+  if (!store->seek(offset))
+    {
+    if (expectedTime.IsValid())
+      {
+      qDebug() << warningPrefix << "failed to seek to position"
+               << offset << "for timestamp" << expectedTime;
+      }
+    else
+      {
+      qDebug() << warningPrefix << "failed to seek to position" << offset;
+      }
+    return {};
+    }
+
+  vxl_int_64 ts;
+  vil_image_view<vxl_byte> vilImage;
+
+  // Read timestamp and image from data stream
+  stream->clear_serialisation_records();
+  vsl_b_read(*stream, ts);
+  if (version < 3)
+    {
+    vsl_b_read(*stream, vilImage);
+    }
+  else
+    {
+    QScopedPointer<vil_file_format> format;
+    char formatMarker;
+    vsl_b_read(*stream, formatMarker);
+    switch (formatMarker)
+      {
+      case 'j':
+      case 'J':
+        format.reset(new vil_jpeg_file_format);
+        break;
+      default:
+        qDebug() << warningPrefix << "unknown compression format"
+                 << formatMarker;
+        return {};
+      }
+    vil_stream* memoryStream = new vil_stream_core();
+    memoryStream->ref();
+    std::vector<char> bytes;
+    vsl_b_read(*stream, bytes);
+    memoryStream->write(&bytes[0], static_cast<vil_streampos>(bytes.size()));
+    vil_image_resource_sptr imageResource =
+      format->make_input_image(memoryStream);
+    if (!imageResource)
+      {
+      qDebug() << warningPrefix << "failed to read frame image at" << offset;
+      memoryStream->unref();
+      return {};
+      }
+    vilImage = imageResource->get_view();
+    memoryStream->unref();
+    }
+
+  if (!vilImage.is_contiguous())
+    {
+    qDebug() << warningPrefix << "cannot convert non-contiguous image";
+    return {};
+    }
+
+  // Convert to vgImage
+  vil_memory_chunk_sptr* chunk =
+    new vil_memory_chunk_sptr(vilImage.memory_chunk());
+  vgImage::Closure cleanup(&vilCleanup, chunk);
+
+  auto image = vgImage{vilImage.top_left_ptr(),
+                       static_cast<int>(vilImage.ni()),
+                       static_cast<int>(vilImage.nj()),
+                       static_cast<int>(vilImage.nplanes()),
+                       static_cast<int>(vilImage.istep()),
+                       static_cast<int>(vilImage.jstep()),
+                       static_cast<int>(vilImage.planestep()),
+                       cleanup};
+
+  return {ts, image};
 }
