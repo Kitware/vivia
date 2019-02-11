cdfd8eb75a9d26895522e7870fda743562e708ea
Matthew Woehlke <matthew.woehlke@kitware.com>
2019-02-11 16:50:25 -0500

    Make fixHeaderGuards a QCoreApplication
    Modify fixHeaderGuards to be a QCoreApplication rather than a
    QApplication. This tool has no need for GUI, and the change will allow
    it to link only to QtCore.

--- Tools/FixHeaderGuards/fixHeaderGuards.cxx
+++ Tools/FixHeaderGuards/fixHeaderGuards.cxx
@@ -1,12 +1,12 @@
 /*ckwg +5
- * Copyright 2013 by Kitware, Inc. All Rights Reserved. Please refer to
+ * Copyright 2019 by Kitware, Inc. All Rights Reserved. Please refer to
  * KITWARE_LICENSE.TXT for licensing information, or contact General Counsel,
  * Kitware, Inc., 28 Corporate Drive, Clifton Park, NY 12065.
  */
 
 #include <cstdio>
 
-#include <QApplication>
+#include <QCoreApplication>
 #include <QFile>
 #include <QFileInfo>
 #include <QRegExp>
@@ -134,7 +134,7 @@ int fixHeaderGuards(const QString& format, const QString& fileName)
 //-----------------------------------------------------------------------------
 int main(int argc, char** argv)
 {
-  QApplication app(argc, argv);
+  QCoreApplication app{argc, argv};
 
   // Set up QTextStream to stderr
   QFile ferr;
