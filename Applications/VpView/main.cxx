bd7ff5bdb943bf20e007f1b193a5379617637f12
Matthew Woehlke <matthew.woehlke@kitware.com>
2019-02-11 17:08:59 -0500

    Remove unneeded QCleanlooksStyle header
    A long, long time ago (apparently, in the very first version ever
    committed), WAMI Viewer forcibly set the widget style to Cleanlooks.
    While it has not done so in a similarly long time, the include never
    went away. Delete it now.
    
    This will be one less stumbling block when we switch to Qt5.

--- Applications/VpView/main.cxx
+++ Applications/VpView/main.cxx
@@ -1,12 +1,11 @@
 /*ckwg +5
- * Copyright 2018 by Kitware, Inc. All Rights Reserved. Please refer to
+ * Copyright 2019 by Kitware, Inc. All Rights Reserved. Please refer to
  * KITWARE_LICENSE.TXT for licensing information, or contact General Counsel,
  * Kitware, Inc., 28 Corporate Drive, Clifton Park, NY 12065.
  */
 
 // QT includes
 #include <QApplication>
-#include <QCleanlooksStyle>
 #include <QSettings>
 
 // QT Extensions includes
